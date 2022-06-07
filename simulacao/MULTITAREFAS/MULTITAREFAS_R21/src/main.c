57c6121be34a1b8f961ef4d79030e51e3291f95c
Michael Strahl <michaelstrahl.sm@gmail.com>
2022-06-07 18:39:36 -0300

    Update EXTINT callback main files

--- simulacao/MULTITAREFAS/MULTITAREFAS_R21/src/main.c
+++ simulacao/MULTITAREFAS/MULTITAREFAS_R21/src/main.c
@@ -58,6 +58,8 @@ void thread3(void);
 void thread4(void);
 void thread5(void);
 void configure_extint_channel(void);
+void configure_extint_callbacks(void);
+void extint_detection_callback(void);
 void extint_polled(void);
 void extint_callback(void);
 
@@ -101,13 +103,23 @@ semaforo_t SemaforoVazio = {TAM_BUFFER,0}; /* declaracao e inicializacao de um s
 /* Configura EXTINT */
 void configure_extint_channel(void)
 {
- struct extint_chan_conf config_extint_chan;
- extint_chan_get_config_defaults(&config_extint_chan);
- config_extint_chan.gpio_pin = BUTTON_0_EIC_PIN;
- config_extint_chan.gpio_pin_mux = BUTTON_0_EIC_MUX;
- config_extint_chan.gpio_pin_pull = EXTINT_PULL_UP;
- config_extint_chan.detection_criteria = EXTINT_DETECT_BOTH;
- extint_chan_set_config(BUTTON_0_EIC_LINE, &config_extint_chan);
+	struct extint_chan_conf config_extint_chan;
+	extint_chan_get_config_defaults(&config_extint_chan);
+	config_extint_chan.gpio_pin = BUTTON_0_EIC_PIN;
+	config_extint_chan.gpio_pin_mux = BUTTON_0_EIC_MUX;
+	config_extint_chan.gpio_pin_pull = EXTINT_PULL_UP;
+	config_extint_chan.detection_criteria = EXTINT_DETECT_BOTH;
+	extint_chan_set_config(BUTTON_0_EIC_LINE, &config_extint_chan);
+}
+void configure_extint_callbacks(void)
+{
+	extint_register_callback(extint_detection_callback,BUTTON_0_EIC_LINE,EXTINT_CALLBACK_TYPE_DETECT);
+	extint_chan_enable_callback(BUTTON_0_EIC_LINE,EXTINT_CALLBACK_TYPE_DETECT);
+}
+void extint_detection_callback(void)
+{
+	bool pin_state = port_pin_get_input_level(BUTTON_0_PIN);
+	port_pin_set_output_level(LED_0_PIN, pin_state);
 }
 
 /*
@@ -148,7 +160,13 @@ int main(void)
 
 	//configure_extint_channel();
 
-	CriaTarefa(extint_polled,"Tarefa Polled",PILHA_TAREFA_1, TAM_PILHA_1,1);
+	//CriaTarefa(extint_polled,"Tarefa Polled",PILHA_TAREFA_1, TAM_PILHA_1,1);
+
+	configure_extint_channel();
+	configure_extint_callbacks();
+	system_interrupt_enable_global();
+
+	CriaTarefa(extint_callback,"Tarefa Callback",PILHA_TAREFA_1, TAM_PILHA_1,1);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
